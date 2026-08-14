78e9a728daf92bb84fde48ae49faa3e3f5cc87b9
Ayush995712 <ayush181217@gmail.com>
2026-08-15 00:18:40 +0530

    updated code to ignore unused variables

--- intermediate/src/medium/rc_shared.rs
+++ intermediate/src/medium/rc_shared.rs
@@ -14,7 +14,7 @@ use std::rc::Rc;
 
 pub fn shared_ownership(value: String) -> (usize, String) {
     let rc_string = Rc::new(value);
-    let clone1 = Rc::clone(&rc_string);
-    let clone2 = Rc::clone(&rc_string);
+    let _clone1 = Rc::clone(&rc_string);
+    let _clone2 = Rc::clone(&rc_string);
     (Rc::strong_count(&rc_string), (*rc_string).clone())
 }
