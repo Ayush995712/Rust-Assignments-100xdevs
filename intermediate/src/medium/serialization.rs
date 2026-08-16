3447227fc0b932a97629ff0c2d4c26cdf4566df4
Ayush995712 <ayush181217@gmail.com>
2026-08-16 15:59:23 +0530

    problem 47 done

--- intermediate/src/medium/serialization.rs
+++ intermediate/src/medium/serialization.rs
@@ -18,10 +18,46 @@ pub struct Record {
 
 impl Record {
     pub fn to_bytes(&self) -> Vec<u8> {
-        todo!()
+        let mut v = Vec::new();
+
+        let lower_byte_id = (self.id & 255u32) as u8;
+        let lowerm_byte_id = ((self.id >> 8) & 255u32) as u8;
+        let upper_byte_id = (self.id >> 24 & 255u32) as u8;
+        let upperm_byte_id = ((self.id >> 16) & 255u32) as u8;
+
+        let lower_byte_value = (self.value & 255u16) as u8;
+        let upper_byte_value = (self.value >> 8 & 255u16) as u8;
+
+        v.push(lower_byte_id);
+        v.push(lowerm_byte_id);
+        v.push(upperm_byte_id);
+        v.push(upper_byte_id);
+
+        v.push(lower_byte_value);
+        v.push(upper_byte_value);
+
+        return v
     }
 
     pub fn from_bytes(data: &[u8]) -> Result<Self, String> {
-        todo!()
+        if data.len() != 6 {
+            return Err("Invalid data length".to_string())
+        }
+
+        let mut id = 0 as u32;
+        let mut value = 0u16;
+        
+        for (i, byte) in data.iter().enumerate() {
+            if i < 4 {
+                let rotation = 8 * i;
+                let id_dup = (*byte as u32) << rotation;
+                id |= id_dup;
+            } else {
+                let rotation = 8 * (i - 4);
+                let val_dup = (*byte as u16) << rotation;
+                value |= val_dup;
+            }
+        }
+        Ok( Self { id, value })
     }
 }
