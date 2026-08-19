d6c850e0d5bb7647d6d17e0927ffb51bd27f1599
Ayush995712 <ayush181217@gmail.com>
2026-08-20 01:44:37 +0530

    problem 60 done

--- intermediate/src/hard/solana_instruction.rs
+++ intermediate/src/hard/solana_instruction.rs
@@ -19,5 +19,20 @@ pub enum Instruction {
 }
 
 pub fn unpack(data: &[u8]) -> Result<Instruction, String> {
-    todo!()
+    if data.is_empty() {
+        return Err("Data can't be empty".to_string())
+    }
+
+    let tag = data[0];
+    if tag == 0u8 {
+        return Ok(Instruction::Initialize);
+    } else if tag == 1u8 {
+        if data.len() != 9 { return Err("Mint amount needs to be 8 bytes".to_string())};
+        return Ok(Instruction::Mint { amount: u64::from_le_bytes(data[1..].try_into().map_err(|_| "Invalid amount bytes".to_string())?)})
+    } else if tag == 2u8 {
+        if data.len() != 9 { return Err("Transfer amount needs to be 8 bytes".to_string())};
+        return Ok(Instruction::Transfer { amount: u64::from_le_bytes(data[1..].try_into().map_err(|_| "Invalid amount bytes".to_string())?) })
+    } else {
+        return Err("Data is not of Instruction type".to_string());
+    }
 }
