a1b1dd94d1fe0f8f2b24766b70ece58d3630f07a
Ayush995712 <ayush181217@gmail.com>
2026-08-20 11:16:44 +0530

    problem 66 done

--- intermediate/src/hard/const_generics_matrix.rs
+++ intermediate/src/hard/const_generics_matrix.rs
@@ -15,10 +15,13 @@ pub struct Matrix<T, const R: usize, const C: usize> {
 
 impl<T: Copy, const R: usize, const C: usize> Matrix<T, R, C> {
     pub fn new(data: [[T; C]; R]) -> Self {
-        todo!()
+        Self { data: data }
     }
 
     pub fn get(&self, r: usize, c: usize) -> Option<&T> {
-        todo!()
+        if r >= R || c >= C {
+            return None
+        };
+        Some(&self.data[r][c])
     }
 }
