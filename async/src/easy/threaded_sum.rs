c9f23bfccce26648ab1211e0eeb373d1168f5cdc
Ayush995712 <ayush181217@gmail.com>
2026-08-21 23:44:57 +0530

    problem 71 done

--- async/src/easy/threaded_sum.rs
+++ async/src/easy/threaded_sum.rs
@@ -9,8 +9,19 @@
     cargo test --test threaded_sum_test
 */
 
-use std::thread;
+use std::{thread};
 
 pub fn threaded_sum(v: Vec<i32>) -> i32 {
-    todo!()
+    let length = v.len();
+    let vec1 = v[..length/2].to_vec();
+    let vec2 = v[length/2..].to_vec();
+    let handle1 = thread::spawn(move || {
+      vec1.iter().sum::<i32>()
+    });
+    let handle2 = thread::spawn(move || {
+      vec2.iter().sum::<i32>()
+    });
+    let sum1 = handle1.join().unwrap();
+    let sum2 = handle2.join().unwrap();
+    sum1 + sum2
 }
