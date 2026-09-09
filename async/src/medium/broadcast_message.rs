370cf74550146fad70b12efa15da3a66a0596d50
Ayush995712 <ayush181217@gmail.com>
2026-09-09 23:36:19 +0530

    problem 89 done

--- async/src/medium/broadcast_message.rs
+++ async/src/medium/broadcast_message.rs
@@ -13,5 +13,26 @@
 use tokio::sync::broadcast;
 
 pub async fn broadcast_demo() -> i32 {
-    todo!()
+    let (tx, _rx) = broadcast::channel(10);
+    let mut rx2 = tx.subscribe();
+    let mut rx3 = tx.subscribe();
+    let mut rx4 = tx.subscribe();
+
+    let res2 = tokio::spawn(async move {
+      let res: i32 = rx2. recv().await.unwrap();
+      res
+    });
+    let res3 = tokio::spawn(async move {
+      let res: i32 = rx3. recv().await.unwrap();
+      res
+    });
+    let res4 = tokio::spawn(async move {
+      let res: i32 = rx4. recv().await.unwrap();
+      res
+    });
+
+    tx.send(10).unwrap();
+
+    res2.await.unwrap() + res3.await.unwrap() + res4.await.unwrap()
+    
 }
