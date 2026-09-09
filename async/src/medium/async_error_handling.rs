a35b0da3d576723ae9aa8d50c33cf9a01a703790
Ayush995712 <ayush181217@gmail.com>
2026-09-09 20:14:40 +0530

    problem 93 done

--- async/src/medium/async_error_handling.rs
+++ async/src/medium/async_error_handling.rs
@@ -11,10 +11,15 @@
     cargo test --test async_error_handling_test
 */
 
+use tokio::time::{sleep, Duration};
+
 pub async fn fetch_data(id: u32) -> Result<String, String> {
-    todo!()
+    if id == 0 { return Err("Invalid ID".to_string())};
+    sleep(Duration::from_millis(5)).await;
+    Ok(format!("Data_{}", id))
 }
 
 pub async fn fetch_and_process(id: u32) -> Result<usize, String> {
-    todo!()
+    let res = fetch_data(id).await?;
+    Ok(res.len())    
 }
