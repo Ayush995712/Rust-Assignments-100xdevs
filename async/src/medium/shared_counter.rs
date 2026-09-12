9243e25938a86b8640293becec4d58f1ae65234c
Ayush995712 <ayush181217@gmail.com>
2026-09-12 13:20:19 +0530

    problem 74 done

--- async/src/medium/shared_counter.rs
+++ async/src/medium/shared_counter.rs
@@ -13,5 +13,22 @@ use std::sync::{Arc, Mutex};
 use std::thread;
 
 pub fn multithreaded_counter() -> i32 {
-    todo!()
+  let counter = Arc::new(Mutex::new(0));
+  let mut handles = vec![];
+
+  for _ in 0..10 {
+    let counter = Arc::clone(&counter);
+    let handle = thread::spawn(move || {
+      let mut num = counter.lock().unwrap();
+      *num += 100;
+    });
+    handles.push(handle);
+  };
+
+  for handle in handles {
+    handle.join().unwrap();
+  };
+
+  let res = *counter.lock().unwrap();
+  res
 }
