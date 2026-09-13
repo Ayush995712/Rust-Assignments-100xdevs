ceefe025283c3d1900b775bf3dcffb85b8fc5d67
Ayush995712 <ayush181217@gmail.com>
2026-09-13 10:32:27 +0530

    problem 79 done

--- async/src/medium/atomic_counter.rs
+++ async/src/medium/atomic_counter.rs
@@ -14,5 +14,20 @@ use std::sync::Arc;
 use std::thread;
 
 pub fn atomic_counter() -> i32 {
-    todo!()
+    let atomic_value = Arc::new(AtomicI32::new(0));
+    let mut handles = vec![];
+
+    for _ in 0..10 {
+      let clone_atomic_value = Arc::clone(&atomic_value);
+      let handle = thread::spawn(move || {
+        clone_atomic_value.fetch_add(100, Ordering::SeqCst);
+      });
+      handles.push(handle);
+    }
+
+    for handle in handles {
+      handle.join().unwrap();
+    }
+
+    atomic_value.load(Ordering::Relaxed)
 }
